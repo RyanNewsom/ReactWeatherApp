494ec4d378dc0738f1ee4eb51c509a7f099c1404
= <rnewsom@msudenver.edu>
2017-12-31 15:37:30 -0700

    added api call to get the weather

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,17 @@
+import axios from 'axios';
+
 const API_KEY = '2c7e4bdb8c17e7bca2cba1c88f2fcca8';
+const BASE_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const COUNTRY_CODE = 'us';
+
+export const FETCH_WEATHER = "FETCH_WEATHER";
+
+export function fetchWeather(city) {
+    const url = `${BASE_URL}&q=${city},${COUNTRY_CODE}`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
